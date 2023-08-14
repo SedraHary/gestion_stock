8d4d4f487b0c481c854f061bcc4e90d95ee4e514
sranaivoson <sedraranaivoson@gmail.com>
2023-08-14 23:15:05 +0300

    Modification et suppression client, Garder les header des tableaux

--- server/repositories/CustomerRepository.js
+++ server/repositories/CustomerRepository.js
@@ -16,8 +16,8 @@ class CustomerRepository {
   async addCustomer(customerName, customerlastName, customerContact, customerCode) {
     try {
       await db.query(`INSERT INTO public.customer (customerid, customername, customerlastname, customer_contact,customerCode) VALUES (DEFAULT, '${customerName}', '${customerlastName}', '${customerContact}', '${customerCode}');`);
-      const result = await db.query('SELECT customerid, customername,customerlastname, customer_contact, customerCode FROM public.customer');
-      return result.rows.map((row) => new Customer(row.customerid, row.customername, row.customerlastname, row.customer_contact, row.customerCode));
+      const result = await db.query('SELECT customerid, customername, customerlastname, customer_contact, customerCode FROM public.customer');
+      return result.rows.map((row) => new Customer(row.customerid, row.customername, row.customerlastname, row.customer_contact, row.customercode));
     } catch (err) {
       console.error('Error fetching customer:', err);
       throw new Error('Internal server error');
@@ -27,8 +27,8 @@ class CustomerRepository {
   async deleteCustomer(customerId) {
     try {
       await db.query(`DELETE FROM public.customer WHERE customerid='${customerId}';`);
-      const result = await db.query('SELECT customerid, customername, customerlastname, customer_contact FROM public.customer');
-      return result.rows.map((row) => new Customer(row.customerid, row.customername, row.customerlastname, row.customer_contact));
+      const result = await db.query('SELECT customerid, customername, customerlastname, customer_contact, customerCode FROM public.customer');
+      return result.rows.map((row) => new Customer(row.customerid, row.customername, row.customerlastname, row.customer_contact, row.customercode));
     } catch (err) {
       console.error('Error fetching Customer:', err);
       throw new Error('Internal server error');
