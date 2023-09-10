85c526adba803c05e5dc79dcf43384ebf0562195
sranaivoson <sedraranaivoson@gmail.com>
2023-09-11 01:19:38 +0300

    correction erreur si remise est null

--- server/repositories/BillRepository.js
+++ server/repositories/BillRepository.js
@@ -18,11 +18,12 @@ class BillRepository {
     try {
       const lastBillNum = await db.query(`SELECT MAX(bill_number) AS lastNumber FROM public.bill;`);
       const newBillNum = isNaN(parseInt(lastBillNum.rows[0].lastnumber)+1)? 1 : parseInt(lastBillNum.rows[0].lastnumber)+1;
-      const result = await db.query(`INSERT INTO public.bill(bill_id, bill_number, bill_total_price, bill_date, remise, id_customer, customer_name) VALUES (DEFAULT, '${newBillNum}', '${totalPrice}', '${date}', '${remise}', '${customerId}', '${customerName}') RETURNING bill_id;`);
+      const remiseData = remise? remise : 0;
+      const result = await db.query(`INSERT INTO public.bill(bill_id, bill_number, bill_total_price, bill_date, remise, id_customer, customer_name) VALUES (DEFAULT, '${newBillNum}', '${totalPrice}', '${date}', '${remiseData}', '${customerId}', '${customerName}') RETURNING bill_id;`);
       
-      for (const articleData of articleDatas) {
-        await db.query(`INSERT INTO public.bill_detail(bill_detail_id, quantity, unite_price, amount, bill_id, article_id) VALUES (DEFAULT, '${articleData.quantity}', '${articleData.price}', '${articleData.total}', '${result.rows[0].bill_id}', ${articleData.idArticle}) ;`);
-      }
+      // for (const articleData of articleDatas) {
+      //   await db.query(`INSERT INTO public.bill_detail(bill_detail_id, quantity, unite_price, amount, bill_id, article_id) VALUES (DEFAULT, '${articleData.quantity}', '${articleData.price}', '${articleData.total}', '${result.rows[0].bill_id}', ${articleData.idArticle}) ;`);
+      // }
     } catch (err) {
       console.error('Error fetching bills:', err);
       throw new Error('Internal server error');
